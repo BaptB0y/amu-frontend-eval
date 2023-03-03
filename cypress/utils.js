08ef78171d93bcf3fc0b2b0c53ac62a0ec71d314
Baptiste Rucin <baptiste.rucin@gmail.com>
2023-03-03 17:29:06 +0100

    first commit

--- cypress/utils.js
+++ cypress/utils.js
@@ -1,9 +1,16 @@
 // L'URL sur laquelle votre application web est visitable (à changer si nécessaire)
 export const BASE_URL = "http://localhost:4200/";
 // L'URL de l'API Supabase à mettre à jour absolument
+<<<<<<< HEAD
+export const API_URL = "https://gdzprwugmxdxnpgztbwt.supabase.co/rest/v1";
+// La clé d'API de votre compte Supabase à mettre à jour absolument
+export const API_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MDUyMjE4MCwiZXhwIjoxOTU2MDk4MTgwfQ.bs4zHK_Hor5Z1ojmZ9MBF_jTvZpiye_DpiUPZcq209A";
+=======
 export const API_URL = "https://wnprumrkcufpgpnwhwor.supabase.co/rest/v1";
 // La clé d'API de votre compte Supabase à mettre à jour absolument
 export const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InducHJ1bXJrY3VmcGdwbndod29yIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzc4NDYxNDYsImV4cCI6MTk5MzQyMjE0Nn0.ejgtl-OE4KEZHq9HCYAcR8RfSfbDbD__YayhFL20-3I";
+>>>>>>> d2a1d4d (First commit)
 
 /**
  * Petite fonction utilitaire qui permet de supprimer tout ce qui se trouve dans les tables customers et invoices
@@ -12,7 +19,7 @@ export const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmF
 export const resetDatabase = () => {
   cy.request({
     method: "DELETE",
-    url: API_URL + "/invoices",
+    url: API_URL + "/invoices?id=gt.0",
     headers: {
       apiKey: API_KEY,
     },
@@ -20,7 +27,7 @@ export const resetDatabase = () => {
 
   cy.request({
     method: "DELETE",
-    url: API_URL + "/customers",
+    url: API_URL + "/customers?id=gt.0",
     headers: {
       apiKey: API_KEY,
     },
